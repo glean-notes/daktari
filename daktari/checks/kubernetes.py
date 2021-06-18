a19174cc0edbd5bb2cccacd559973b766801bb8e
mcmarkj <mark@markmcw.uk>
2021-06-18 09:41:57 +0100

    PR Feedback

--- daktari/checks/kubernetes.py
+++ daktari/checks/kubernetes.py
@@ -8,7 +8,7 @@ from daktari.os import OS
 
 
 class KubectlInstalled(Check):
-    def __init__(self, minimum_version: float):
+    def __init__(self, minimum_version: Optional[float] = None):
         self.minimum_version = minimum_version
         self.name = "Kubectl.installed"
         self.suggestions = {
@@ -49,12 +49,7 @@ class KubectlContextExists(Check):
     def __init__(self, context_name: str, provision_command: str = None):
         self.context_name = context_name
         self.name = f"kubectl.contextExists.{context_name}"
-
-        if provision_command is None:
-            self.suggestions = {
-                OS.GENERIC: f"""A kubectl context is missing, :
-                               <cmd>{provision_command}</cmd>"""
-            }
+        self.suggestions = {OS.GENERIC: provision_command}
 
     def check(self) -> CheckResult:
         output = get_stdout("kubectl config get-contexts")
