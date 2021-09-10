2f0f353bc0c354ce40a74ee826761aa7f6abc00f
Dan Fry <dan@sonocent.com>
2021-09-10 10:23:54 +0100

    Remove duplicated semver parsing function.

--- daktari/version_utils.py
+++ daktari/version_utils.py
@@ -2,8 +2,8 @@ from semver import VersionInfo
 from typing import Optional
 
 
-def try_parse_semver(version_str: str) -> Optional[VersionInfo]:
+def try_parse_semver(version_str: Optional[str]) -> Optional[VersionInfo]:
     try:
-        return VersionInfo.parse(version_str)
+        return None if version_str is None else VersionInfo.parse(version_str)
     except ValueError:
         return None
