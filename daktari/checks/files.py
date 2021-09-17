8065a04ddb621e2ab98eb9408213e519ef09d565
Dan Fry <dan@sonocent.com>
2021-09-17 12:33:55 +0100

    Add FilesExist check.

--- daktari/checks/files.py
+++ daktari/checks/files.py
@@ -1,16 +1,25 @@
 from os.path import expanduser
+from typing import List
 
 from daktari.check import Check, CheckResult
 from daktari.file_utils import file_exists
 from daktari.os import OS
 
 
-class FileExists(Check):
+class FilesExist(Check):
+    name = "files.exist"
+    file_paths: List[str] = []
+    pass_fail_message = ""
+
+    def check(self) -> CheckResult:
+        files_exist = all([file_exists(expanduser(file_path)) for file_path in self.file_paths])
+        return self.verify(files_exist, self.pass_fail_message)
+
+
+class FileExists(FilesExist):
     name = "file.exists"
 
     def __init__(self, file_path: str, suggestion: str):
-        self.file_path = file_path.replace("~", expanduser("~"))
+        self.file_paths = [file_path]
+        self.pass_fail_message = f"{file_path} is <not/> present"
         self.suggestions = {OS.GENERIC: suggestion}
-
-    def check(self) -> CheckResult:
-        return self.verify(file_exists(self.file_path), f"{self.file_path} is <not/> present")
