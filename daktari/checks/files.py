cdeccae92639c4dda3168d45277244fe9444b53f
Dan Fry <dan@sonocent.com>
2021-09-23 12:33:16 +0100

    Add check for the existence of one or more directories.

--- daktari/checks/files.py
+++ daktari/checks/files.py
@@ -2,7 +2,7 @@ from os.path import expanduser
 from typing import List
 
 from daktari.check import Check, CheckResult
-from daktari.file_utils import file_exists
+from daktari.file_utils import dir_exists, file_exists
 from daktari.os import OS
 
 
@@ -22,4 +22,23 @@ class FileExists(FilesExist):
     def __init__(self, file_path: str, suggestion: str):
         self.file_paths = [file_path]
         self.pass_fail_message = f"{file_path} is <not/> present"
+        self.suggestions = {OS.GENERIC: suggestion}
+
+
+class DirsExist(Check):
+    name = "directories.exist"
+    dir_paths: List[str] = []
+    pass_fail_message = ""
+
+    def check(self) -> CheckResult:
+        dirs_exist = all([dir_exists(expanduser(dir_path)) for dir_path in self.dir_paths])
+        return self.verify(dirs_exist, self.pass_fail_message)
+
+
+class DirExists(DirsExist):
+    name = "dir.exists"
+
+    def __init__(self, dir_path: str, suggestion: str):
+        self.dir_paths = [dir_path]
+        self.pass_fail_message = f"{dir_path} is <not/> present"
         self.suggestions = {OS.GENERIC: suggestion}
