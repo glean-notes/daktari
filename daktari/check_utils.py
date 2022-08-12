02d4534c004e687b50512139db1a9ac43f6afb0c
Alyssa <alyssa.burlton@glean.co>
2022-08-12 15:04:12 +0100

    Simplify slightly

--- daktari/check_utils.py
+++ daktari/check_utils.py
@@ -21,5 +21,4 @@ def _get_all_dependent_check_names_recursive(check: Union[Check, Type[Check]], p
 
     sub_dependents = [_get_all_dependent_check_names_recursive(dep, prev_parents) for dep in check.depends_on]
     flat_sub_dependents = {item for subset in sub_dependents for item in subset}
-    result = flat_sub_dependents.union(dependents)
-    return result
+    return flat_sub_dependents.union(dependents)
