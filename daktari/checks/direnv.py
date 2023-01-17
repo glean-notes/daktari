700fdb2e242d0a99a6ec7e0e7ef1060bf2e493f1
Matthew Kandiah <matthew.kandiah@glean.co>
2023-01-17 11:50:25 +0000

    fix bad name, make argument non-optional again

--- daktari/checks/direnv.py
+++ daktari/checks/direnv.py
@@ -30,8 +30,8 @@ class DirenvAllowed(Check):
     name = "direnv.allowed"
     depends_on = [DirenvInstalled]
 
-    def __init__(self, parent_file_name: Optional[str] = None):
-        self.file_name = parent_file_name
+    def __init__(self, parent_file_name: str):
+        self.parent_file_name = parent_file_name
         self.suggestions = {OS.GENERIC: "<cmd>direnv allow .</cmd>"}
 
     def check(self) -> CheckResult:
