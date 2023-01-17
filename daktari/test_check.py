e5f0edf32bca6542e8c4792150336086e449ebe0
Matthew Kandiah <matthew.kandiah@glean.co>
2023-01-17 16:09:39 +0000

    unit tests - test dependencies get added and that existing dependencies not overwritten

--- daktari/test_check.py
+++ daktari/test_check.py
@@ -3,7 +3,7 @@ import unittest
 from semver import VersionInfo
 
 from daktari.check import CheckResult, CheckStatus
-from daktari.test_check_factory import DummyCheck
+from daktari.test_check_factory import DummyCheck, DummyCheck2
 
 INSTALLED_VERSION = VersionInfo(10, 3, 1)
 
@@ -38,6 +38,14 @@ class TestConfig(unittest.TestCase):
             "Application", INSTALLED_VERSION, required_version=">=10.0.0", recommended_version="<11.0.0"
         )
         self._verify_result(result, CheckStatus.PASS, "Application version is 10.3.1")
+
+    def test_with_dependencies_adds_dependency(self):
+        check = DummyCheck(depends_on=None).with_dependencies(DummyCheck, DummyCheck2)
+        self.assertEqual(check.depends_on, [DummyCheck, DummyCheck2])
+
+    def test_with_dependencies_keeps_existing_dependencies(self):
+        check = DummyCheck(depends_on=[DummyCheck]).with_dependencies(DummyCheck2)
+        self.assertEqual(check.depends_on, [DummyCheck, DummyCheck2])
 
     def _verify_result(self, result: CheckResult, expected_status: CheckStatus, expected_message: str):
         self.assertEqual(result, CheckResult("check.name", expected_status, expected_message, {}))
--- daktari/test_check_factory.py
+++ daktari/test_check_factory.py
@@ -10,3 +10,14 @@ class DummyCheck(Check):
 
     def check(self) -> CheckResult:
         return self.passed("no check")
+
+
+class DummyCheck2(Check):
+    def __init__(self, name: str = "check.name", depends_on=None):
+        if depends_on is None:
+            depends_on = []
+        self.name = name
+        self.depends_on = depends_on
+
+    def check(self) -> CheckResult:
+        return self.passed("no check")
