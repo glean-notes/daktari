829853b681ef8376b806a1b6d7326dad51cead7d
Matthew Kandiah <matthew.kandiah@glean.co>
2023-08-02 16:49:08 +0100

    fix

--- daktari/checks/conan.py
+++ daktari/checks/conan.py
@@ -15,20 +15,17 @@ class ConanInstalled(Check):
         self.suggestions = {OS.GENERIC: "Install conan: <cmd>pip install conan</cmd>"}
 
     def check(self) -> CheckResult:
-        installed_version = get_simple_cli_version("direnv")
-        return self.validate_semver_expression(
-            "conan", installed_version, self.required_version, self.recommended_version
-        )
+        return self.verify_install("conan")
 
 
 class ConanProfileDetected(Check):
     name = "conan.profileDetected"
-    depends_on = [ConanInstalled]
 
     def __init__(self, expected_string: str):
         self.suggestions = {OS.GENERIC: "<cmd>conan profile detect</cmd>"}
         self.expected_string = expected_string
+        self.depends_on = [ConanInstalled]
 
     def check(self) -> CheckResult:
         expected_profile_detected = self.expected_string in get_stdout("conan profile list")
-        return self.verify(expected_profile_detected, f"conan profile ${self.expected_string} <not /> detected")
+        return self.verify(expected_profile_detected, f"conan profile {self.expected_string} <not/> detected")
