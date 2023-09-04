6d85f2fc747abc8f9cb995180d6bd3466e05ab1d
Chris Wardell <chris.wardell@glean.co>
2023-09-04 15:48:23 +0100

    Adding clipboard test

--- daktari/test_check_factory.py
+++ daktari/test_check_factory.py
@@ -2,13 +2,24 @@ from daktari.check import Check, CheckResult
 
 
 class DummyCheck(Check):
-    def __init__(self, name: str = "check.name", depends_on=None, succeed: bool = True):
+    def __init__(
+        self,
+        name: str = "check.name",
+        depends_on=None,
+        succeed: bool = True,
+        suggestions=None,
+        command_suggestions=None,
+    ):
         if depends_on is None:
             depends_on = []
         self.name = name
         self.depends_on = depends_on
         self.was_run = False
         self.succeed = succeed
+        if suggestions is not None:
+            self.suggestions = suggestions
+        if command_suggestions is not None:
+            self.command_suggestions = command_suggestions
 
     def check(self) -> CheckResult:
         self.was_run = True
