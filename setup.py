bc7052d3aa3bc9a5e523c2d6f8f63a6349ede663
BumpVersion Action <bumpversion@github-actions>
2023-09-11 10:47:14 +0000

    Bump version: 0.0.135 → 0.0.136

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="daktari",
-    version="0.0.135",
+    version="0.0.136",
     description="Assist in setting up and maintaining developer environments",
     long_description=README,
     long_description_content_type="text/markdown",
