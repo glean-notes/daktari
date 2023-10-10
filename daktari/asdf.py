6373432742dd50106273da72c741ed420570e9e4
Matt Russell <matt.russell@glean.co>
2023-10-10 11:31:08 +0100

    add get_tool_version utility

---
+++ daktari/asdf.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+def get_tool_version_from_string(tool: str, file_content: str) -> Optional[str]:
+    lines = file_content.split("\n")
+    for line in lines:
+        clean_line = line.split("#")[0].strip()
+        if clean_line.startswith(tool + " "):
+            return clean_line.split()[1]
+    return None
+
+def get_tool_versione(tool: str) -> Optional[str]:
+    with open('.tool-versions', 'r') as f:
+        file_content = f.read()
+    return get_tool_version_from_string(tool, file_content)
