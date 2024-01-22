346b633946d265869007bfb03b7c98b0bf779043
Simon Jackson <simon.jackson@sonocent.com>
2024-01-22 11:58:15 +0000

    Add check for Conan remotes.

--- daktari/checks/conan.py
+++ daktari/checks/conan.py
@@ -1,3 +1,5 @@
+import json
+import logging
 from typing import Optional
 
 from daktari.check import Check, CheckResult
@@ -29,3 +31,35 @@ class ConanProfileDetected(Check):
         output = get_stdout("conan profile list")
         expected_profile_detected = output is not None and self.expected_string in output
         return self.verify(expected_profile_detected, f"conan profile {self.expected_string} <not/> detected")
+
+
+class ConanRemoteDetected(Check):
+    name = "conan.remoteDetected"
+
+    def __init__(self, remote_name: str, remote_url: str):
+        self.suggestions = {OS.GENERIC: f"<cmd>conan remote add {remote_name} {remote_url} </cmd>"}
+        self.remote_name = remote_name
+        self.remote_url = remote_url
+        self.depends_on = [ConanInstalled]
+
+    def check(self) -> CheckResult:
+        output = get_stdout("conan remote list -f json")
+        if output is None:
+            return self.failed("No conan remotes configured for the current user.")
+        remote_json = json.loads(output)
+        remote = next(filter(lambda remote_details: remote_details.get("name") == self.remote_name, remote_json), None)
+        if remote is None:
+            return self.failed(f"{self.remote_name} is not configured for the current user.")
+
+        configured_url = remote["url"].strip("/")
+        logging.debug(f"{self.remote_name} conan remote is configured with URL {configured_url}.")
+
+        if configured_url != self.remote_url:
+            return self.failed(
+                f"{self.remote_name} conan remote is configured with URL {configured_url}, expected {self.remote_url}"
+            )
+
+        if not remote["enabled"]:
+            return self.failed(f"{self.remote_name} conan remote is not enabled.")
+
+        return self.passed(f"{self.remote_name} conan remote is configured for the current user.")
