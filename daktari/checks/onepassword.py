487960074d5e17fd666c2fa9b3154c0b3561b4d0
Ambrose Gladwell <ambrose.gladwell@glean.co>
2024-05-13 23:53:41 +0100

    Update onepassword account configured check for updated onepassword api

--- daktari/checks/onepassword.py
+++ daktari/checks/onepassword.py
@@ -38,24 +38,18 @@ class OnePasswordAccountConfigured(Check):
     def __init__(self, account_shorthand: str):
         self.account_shorthand = account_shorthand
         self.suggestions = {
-            OS.GENERIC: f"<cmd>op signin {account_shorthand}.1password.com <your-email-here></cmd>",
+            OS.GENERIC: f"<cmd>op signin --account {account_shorthand}.1password.com</cmd>",
         }
 
     def check(self) -> CheckResult:
-        home = str(Path.home())
-        possible_paths = [f"{home}/.op/config", f"{home}/.config/op/config"]
-
-        for config_path in possible_paths:
-            if file_exists(config_path):
-                op_config = json.loads(open(config_path).read())
-                accounts = op_config.get("accounts", [])
-                repo = next(filter(lambda account: account.get("shorthand") == self.account_shorthand, accounts), None)
-                if repo is None:
+            try:
+                output = get_stdout("op account list")
+                if f"{account_shorthand}.1password.com" in output:
+                    return self.passed(f"{self.account_shorthand} is configured with OP CLI for the current user")
+                else:
                     return self.failed(f"{self.account_shorthand} is not configured with OP CLI for the current user")
-
-                return self.passed(f"{self.account_shorthand} is configured with OP CLI for the current user")
-
-        return self.failed("No 1Password config appears to be present on this machine.")
+            except subprocess.CalledProcessError:
+                return self.failed("1Password CLI command failed. Make sure it's installed and configured.")
 
 
 def account_exists(path: str, account_shorthand: str) -> bool:
