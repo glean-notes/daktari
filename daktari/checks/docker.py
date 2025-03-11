3658239ac296442f0d5f25d8c9f33b5152c1d01c
Chris Wardell <chris.wardell@glean.co>
2025-03-11 10:56:05 +0000

    Adding Orbstack installed and Docker Desktop not installed checks

--- daktari/checks/docker.py
+++ daktari/checks/docker.py
@@ -2,11 +2,12 @@ import logging
 import re
 from typing import Optional
 
+from daktari.file_utils import dir_exists
 from semver import VersionInfo
 
 from daktari.check import Check, CheckResult
-from daktari.command_utils import get_stdout
-from daktari.os import OS
+from daktari.command_utils import can_run_command, get_stdout
+from daktari.os import OS, detect_os
 from daktari.version_utils import try_parse_semver
 
 
@@ -48,3 +49,39 @@ class DockerComposeInstalled(Check):
 
     def check(self) -> CheckResult:
         return self.verify_install("docker-compose")
+
+
+def get_orbstack_version() -> Optional[VersionInfo]:
+    raw_output = get_stdout("orb version")
+    if raw_output:
+        match = re.match("Version: ([0-9.]+) ", raw_output)
+        if match:
+            version = try_parse_semver(match.group(1))
+            logging.debug(f"Orbstack version: {version}")
+            return version
+    return None
+
+class OrbStackInstalled(Check):
+    name = "orbstack.installed"
+    suggestions = {
+        OS.GENERIC: "Install orbstack: https://docs.orbstack.dev/install and sign in."
+    }
+
+    def __init__(self, required_version: Optional[str] = None):
+        self.required_version = required_version
+
+
+    def check(self) -> CheckResult:
+        installed_version = get_orbstack_version()
+        return self.validate_semver_expression("Orbstack", installed_version, self.required_version)
+
+
+class DockerDesktopNotInstalled(Check):
+    name = "docker-desktop.not-installed"
+    run_on = OS.OS_X
+    suggestions = {
+        OS.OS_X: "Uninstall Docker Desktop. Open Applications and delete Docker.app",
+    }
+
+    def check(self) -> CheckResult:
+        return self.verify(not dir_exists("/Applications/Docker.app"), "Docker Desktop should not be installed")
