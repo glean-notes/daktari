ad8a61cd8b1a555eebcfbf9124ef73cd1024cb19
BumpVersion Action <bumpversion@github-actions>
2025-03-11 12:31:23 +0000

    Bump version: 0.0.249 → 0.0.250

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ In the root of the project repository, create a `.daktari.py` configuration file
 ```python
 from daktari.checks.git import *
 
-version = "0.0.249"
+version = "0.0.250"
 title = "My Project"
 
 checks = [
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name="daktari",
-    version="0.0.249",
+    version="0.0.250",
     description="Assist in setting up and maintaining developer environments",
     long_description=README,
     long_description_content_type="text/markdown",
